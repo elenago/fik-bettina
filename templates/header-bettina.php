369de68f5c9cb7f7df47d268a07834c3f1959ded
elenago <elenagomezcaballero@gmail.com>
2014-03-12 17:35:38 +0100

    cambios menu header social

--- templates/header-bettina.php
+++ templates/header-bettina.php
@@ -3,18 +3,18 @@
     <div class="lang may col-lg-7">
       <?php
         if (has_nav_menu('language-menu')) :
-          wp_nav_menu(array('theme_location' => 'language-menu', 'menu_class' => 'nav nav-pills'));
+          wp_nav_menu(array('theme_location' => 'language-menu', 'menu_class' => 'nav nav-pills lang-menu pull-right'));
         endif;
       ?>
     </div>
     <div class="may col-lg-3">
       <?php
         if (has_nav_menu('blogshop-menu')) :
-          wp_nav_menu(array('theme_location' => 'blogshop-menu', 'menu_class' => 'nav nav-pills'));
+          wp_nav_menu(array('theme_location' => 'blogshop-menu', 'menu_class' => 'nav nav-pills blog-menu'));
         endif;
       ?>
     </div>
-    <div class="social pull-right col-lg-2">      
+    <div class="social col-lg-2">      
       <?php
         if (has_nav_menu('social-menu')) :
           wp_nav_menu(array('theme_location' => 'social-menu', 'menu_class' => 'nav nav-pills'));
