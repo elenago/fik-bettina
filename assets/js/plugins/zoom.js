0e0f2e369e886f133f51e33b608cce9a83dc02f3
elenago <elenagomezcaballero@gmail.com>
2014-03-28 14:36:02 +0100

    zoom imagen

--- assets/js/plugins/zoom.js
+++ assets/js/plugins/zoom.js
@@ -2,13 +2,13 @@
 * zoom.js is a JS enabling magnifying glass effect on product images and gallery effect.
 */
 $( document ).ready(function() {
-    $("#prod-img").elevateZoom({tint:true, tintColour:'#ccc', tintOpacity:0.5, zoomWindowFadeIn: 500, zoomWindowFadeOut: 500, lensFadeIn: 200, lensFadeOut: 300, easing : true});
+    $("#prod-img").elevateZoom({zoomWindowWidth:458, zoomWindowHeight:656, zoomWindowOffetx:472, tint:true, tintColour:'#ccc', tintOpacity:0.5, zoomWindowFadeIn: 500, zoomWindowFadeOut: 500, lensFadeIn: 200, lensFadeOut: 300, easing : true});
 
     $(".product-image-thumbnails a").click(function(event) {
         event.stopPropagation();
         $("#prod-img").attr("src", $(this).attr("data-zoom-image"));
         // Add zoom to the new image
-        $("#prod-img").elevateZoom({tint:true, tintColour:'#ccc', tintOpacity:0.5, zoomWindowFadeIn: 500, zoomWindowFadeOut: 500, lensFadeIn: 200, lensFadeOut: 300, easing : true});
+        $("#prod-img").elevateZoom({zoomWindowWidth:458, zoomWindowHeight:656, zoomWindowOffetx:472, tint:true, tintColour:'#ccc', tintOpacity:0.5, zoomWindowFadeIn: 500, zoomWindowFadeOut: 500, lensFadeIn: 200, lensFadeOut: 300, easing : true});
         // prevent href
         return false;
     });
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,4 +1,4 @@
-<?php if(!is_page('Home')): ?>
+<?php if(!is_page('Home') && !is_post_type_archive('fik_product') && !is_front_page() && !is_home()): ?>
 <div class="page-header">
   <h1>
     <?php echo roots_title(); ?>
