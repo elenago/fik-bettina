d28fec5e31a75d39a2b4b1c6ac49ed2ec1722666
minayaserrano <minayaserrano@gmail.com>
2014-04-01 14:19:28 +0200

    12 columnas en home

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
-<article class="col-sm-6 contentstatic">
+<?php if(is_home()) { $bootstrapcolumns = '12'; } else { $bootstrapcolumns = "6"; } ?>
+<article class="col-sm-<?php echo $bootstrapcolumns; ?> contentstatic">
 <?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page'); ?>
 </article>
