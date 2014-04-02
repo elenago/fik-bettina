42907f919adf158aa66a7d79d2b367d90bd77dbb
Bill Murphy <murphy@litio.net>
2014-04-02 20:21:35 +0200

    Reorder thumbnails html

--- templates/content.php
+++ templates/content.php
@@ -1,11 +1,11 @@
 <?php if(get_post_type == 'fik_product' || is_tax('store-section')): ?>
 
-    <div class="col-md-6 no-padding-left">
-        <div class="product-image-frame">
+    <div class="col-md-6 no-padding-left product-image-frame">
+
         <?php the_post_thumbnail('460-thumbnail', array('class'=>'img-responsive')); ?>
         <h2 class="product-title" style="margin-top: 10px"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
         <div class="product-price"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_fik_price(); ?></a></div>
-        </div>
+
     </div>
 
 <?php else: ?>
