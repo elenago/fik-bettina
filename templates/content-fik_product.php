2b64593ddeee4614b97c010d3240ee32dda8d50f
Bill Murphy <murphy@litio.net>
2014-04-02 16:59:54 +0200

    Thumbnails are larger for zoom

--- templates/content-fik_product.php
+++ templates/content-fik_product.php
@@ -17,7 +17,7 @@
                         // We print the post thumbnail (if it exists) with a maximum size of 620px x 9999px:
                        // the_post_thumbnail('post-thumbnail',array('data-zoom-image' => array_shift(array_values(wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'large' ))),'itemprop' => "image"));                
                     ?>
-                    <?php the_post_thumbnail('460-thumbnail',array('class'=>'img-responsive', 'id'=>'prod-img')) ?>
+                    <?php the_post_thumbnail('460-thumbnail',array('class'=>'img-responsive', 'id'=>'prod-img', 'data-zoom-image' => array_shift(array_values(wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'large' ))))) ?>
                 </div>
                 <?php 
                 // this function outputs a <ul> with class="product-image-thumbnails" where each <li> is a thumbnil that links to a biger image (sizes specified in function). 
@@ -33,7 +33,7 @@
             <div itemprop="description" class="entry-content">
                 <?php echo $post->post_content; ?>
 
-                <?php the_product_gallery_thumbnails('150-thumbnail', array('class'=>'img-responsive'), '460-thumbnail'); ?>
+                <?php the_product_gallery_thumbnails('150-thumbnail', array('class'=>'img-responsive'), 'full'); ?>
                 <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
             </div><!-- .entry-content -->
             <div class="price-and-purchase">
