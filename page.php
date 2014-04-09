fe5ee562b9aa735cb4c6fa79519e7cac77a3f620
elenago <elenagomezcaballero@gmail.com>
2014-04-09 13:00:36 +0200

    cambios 4 reunión, margen titulo, color banda producto, color botón...

--- page.php
+++ page.php
@@ -1,5 +1,4 @@
 <?php if(is_front_page()) { $bootstrapcolumns = '12'; } else { $bootstrapcolumns = "6"; } ?>
 <article <?php if(is_front_page()) { echo 'id="homearticle"'; } ?> class="col-sm-<?php echo $bootstrapcolumns; ?> contentstatic">
-<?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page'); ?>
 </article>
