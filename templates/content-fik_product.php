7782f8fb52dd3d3db649de46e6f6754b148da831
minayaserrano <minayaserrano@gmail.com>
2014-04-28 17:43:29 +0200

    Zoom image plugin to full size

--- templates/content-fik_product.php
+++ templates/content-fik_product.php
@@ -16,7 +16,7 @@
                         // We print the post thumbnail (if it exists) with a maximum size of 620px x 9999px:
                        // the_post_thumbnail('post-thumbnail',array('data-zoom-image' => array_shift(array_values(wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'large' ))),'itemprop' => "image"));                
                     ?>
-                    <?php the_post_thumbnail('460-thumbnail',array('class'=>'img-responsive', 'id'=>'prod-img', 'data-zoom-image' => array_shift(array_values(wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'large' ))))) ?>
+                    <?php the_post_thumbnail('460-thumbnail',array('class'=>'img-responsive', 'id'=>'prod-img', 'data-zoom-image' => array_shift(array_values(wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'full' ))))) ?>
                 </div>
                 <?php 
                 // this function outputs a <ul> with class="product-image-thumbnails" where each <li> is a thumbnil that links to a biger image (sizes specified in function). 
