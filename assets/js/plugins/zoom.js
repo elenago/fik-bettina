858275036f0e60f76dc461824efe92197d0b2b05
minayaserrano <minayaserrano@gmail.com>
2014-05-06 14:17:44 +0200

    Fix fullsize image

--- assets/js/plugins/zoom.js
+++ assets/js/plugins/zoom.js
@@ -23,10 +23,9 @@ $( document ).ready(function() {
             prevEffect : 'none',
             afterLoad  : function () {
                 $.extend(this, {
-                    aspectRatio : false,
+                    aspectRatio : true,
                     type    : 'html',
                     width   : '100%',
-                    height  : '100%',
                     content : '<div class="fancybox-image" style="background-image:url(' + this.href + '); background-size: cover; background-position:50% 50%;background-repeat:no-repeat;height:100%;width:100%;" /></div>'
                 });
             }
