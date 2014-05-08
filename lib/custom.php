f1c4f3b4a86a297e8241c846ceb13abf75222c96
Bill Murphy <murphy@litio.net>
2014-05-08 14:31:22 +0200

    Bigger thumbnails, less pixelated

--- lib/custom.php
+++ lib/custom.php
@@ -41,7 +41,7 @@ add_theme_support( 'post-thumbnails' );
 
 function bettina_thumb() {
     add_image_size('460-thumbnail', 460, 9999, false); //square thumbnail
-    add_image_size('150-thumbnail', 150, 9999, false); //square thumbnail
+    add_image_size('250-thumbnail', 250, 9999, false); //square thumbnail
 }
 
 add_action('after_setup_theme','bettina_thumb');
