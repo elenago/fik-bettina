f9e31b23d3423396ba564d376c87a630f6972f24
minayaserrano <minayaserrano@gmail.com>
2014-05-12 23:57:11 +0200

    Clean zoom script

--- assets/js/plugins/zoom.js
+++ assets/js/plugins/zoom.js
@@ -10,13 +10,10 @@ function bettinazoom() {
     $("#prod-img").elevateZoom({responsive: true, scrollZoom:true, gallery:'thumbnails-gallery', zoomWindowWidth:$("#prod-img").width(), zoomWindowHeight:$("#prod-img").height(), zoomWindowOffetx: 15, tint:true, tintColour:'#ccc', tintOpacity:0.5, zoomWindowFadeIn: 500, zoomWindowFadeOut: 500, lensFadeIn: 200, lensFadeOut: 300, easing : true});
 
     $(".product-image-thumbnails a").click(function(event) {
+        event.preventDefault();
+        if($(window).width() < 993) return false;
         $("#prod-img").attr("src", $(this).attr("data-zoom-image"));
         $("#prod-img").data("zoom-image", $(this).data("zoom-image"));
-        if($(window).width() < 993) return false;
-        // Add zoom to the new image
-        $("#prod-img").elevateZoom({responsive: true, scrollZoom:true, gallery:'thumbnails-gallery', zoomWindowWidth:$("#prod-img").width(), zoomWindowHeight:$("#prod-img").height(), zoomWindowOffetx: 15, tint:true, tintColour:'#ccc', tintOpacity:0.5, zoomWindowFadeIn: 500, zoomWindowFadeOut: 500, lensFadeIn: 200, lensFadeOut: 300, easing : true});
-        // prevent href
-        return false;
     });
 
     //pass the images to Fancybox
@@ -36,11 +33,6 @@ function bettinazoom() {
                 });
             }
         });
-
-        if($(window).width() < 993) return false;
-        $("#prod-img").elevateZoom({responsive: true, scrollZoom:true, gallery:'thumbnails-gallery', zoomWindowWidth:$("#prod-img").width(), zoomWindowHeight:$("#prod-img").height(), zoomWindowOffetx: 15, tint:true, tintColour:'#ccc', tintOpacity:0.5, zoomWindowFadeIn: 500, zoomWindowFadeOut: 500, lensFadeIn: 200, lensFadeOut: 300, easing : true});
-
-        return false;
     });
 }
 
